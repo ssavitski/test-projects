8a6a54b45248612c7522b5e9e0523a77dfae40b6
ssavitski <ssavitski@thumbtack.net>
2016-05-24 19:14:51 +0300

    Change initialization files

--- recipe-book/webpack.config.js
+++ recipe-book/webpack.config.js
@@ -1,28 +1,19 @@
-var webpack = require("webpack");
+/**
+ * @author: @AngularClass
+ */
 
-module.exports = {
-  entry: {
-    "vendor": "./app/vendor",
-    "app": "./app/boot"
-  },
-  output: {
-    path: __dirname,
-    filename: "./dist/[name].bundle.js"
-  },
-  resolve: {
-    extensions: ['', '.js', '.ts']
-  },
-  devtool: 'source-map',
-  module: {
-    loaders: [
-      {
-        test: /\.ts/,
-        loaders: ['ts-loader'],
-        exclude: /node_modules/
-      }
-    ]
-  },
-  plugins: [
-    new webpack.optimize.CommonsChunkPlugin(/* chunkName= */"vendor", /* filename= */"./dist/vendor.bundle.js")
-  ]
+// Look in ./config folder for webpack.dev.js
+switch (process.env.NODE_ENV) {
+  case 'prod':
+  case 'production':
+    module.exports = require('./config/webpack.prod');
+    break;
+  case 'test':
+  case 'testing':
+    module.exports = require('./config/webpack.test');
+    break;
+  case 'dev':
+  case 'development':
+  default:
+    module.exports = require('./config/webpack.dev');
 }

a1fb7337e9ff3d708ec2e4fa87fb4d464bcdf495
ssavitski <ssavitski@thumbtack.net>
2016-05-25 18:51:44 +0300

    Breakpoint in working process: setting services

--- recipe-book/src/app/app.component.ts
+++ recipe-book/src/app/app.component.ts
@@ -3,6 +3,9 @@
  */
 import { Component, ViewEncapsulation } from '@angular/core';
 import { RouteConfig, Router } from '@angular/router-deprecated';
+import { CollapseDirective } from 'ng2-bootstrap/ng2-bootstrap';
+import { RecipeComponent } from './recipe-book/recipe.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
 import { AppState } from './app.service';
 import { Home } from './home';
@@ -16,46 +19,68 @@ import { RouterActive } from './router-active';
   selector: 'app',
   pipes: [ ],
   providers: [ ],
-  directives: [ RouterActive ],
+  directives: [ RouterActive, CollapseDirective ],
   encapsulation: ViewEncapsulation.None,
   styles: [
     require('./app.css')
   ],
   template: `
-    <span router-active>
-      <button [routerLink]=" ['Index'] ">
-        Index
-      </button>
-    </span>
-    <span router-active>
-      <button [routerLink]=" ['Home'] ">
-        Home
-      </button>
-    </span>
-    <span router-active>
-      <button [routerLink]=" ['About'] ">
-        About
-      </button>
-    </span>
+    <div class="container-fluid container">
+      <nav class="navbar navbar-default">
+        <div class="container-fluid container">
+          <!-- Brand and toggle get grouped for better mobile display -->
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" (click)="isCollapsed = !isCollapsed">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" [routerLink]="['Recipes']">Recipe Book</a>
+          </div>
 
-    <main>
-      <router-outlet></router-outlet>
-    </main>
+          <!-- Collect the nav links, forms, and other content for toggling -->
+          <div class="collapse navbar-collapse" [collapse]="isCollapsed">
+            <ul class="nav navbar-nav">
+              <li class="active"><a [routerLink]="['Recipes']">Recipes <span class="sr-only">(current)</span></a></li>
+              <li><a  [routerLink]="['Shopping-list']">Shopping list</a></li>
+            </ul>
+          </div><!-- /.navbar-collapse -->
+        </div><!-- /.container-fluid -->
+      </nav>
 
-    <pre class="app-state">this.appState.state = {{ appState.state | json }}</pre>
+      <span router-active>
+        <button [routerLink]=" ['Home'] ">
+          Home
+        </button>
+      </span>
+      <span router-active>
+        <button [routerLink]=" ['About'] ">
+          About
+        </button>
+      </span>
+
+      <main>
+        <router-outlet></router-outlet>
+      </main>
+
+      <pre class="app-state">this.appState.state = {{ appState.state | json }}</pre>
+    </div>
   `
 })
 @RouteConfig([
-  { path: '/',      name: 'Index', component: Home, useAsDefault: true },
-  { path: '/home',  name: 'Home',  component: Home },
+  { path: '/home',  name: 'Home',  component: Home,  },
   // Async load a component using Webpack's require with es6-promise-loader and webpack `require`
-  { path: '/about', name: 'About', loader: () => require('es6-promise!./about')('About') }
+  { path: '/about', name: 'About', loader: () => require('es6-promise!./about')('About') },
+  { path: '/recipes', name: 'Recipes', component: RecipeComponent, useAsDefault: true },
+  { path: '/shopping-list', name: 'Shopping-list', component: ShoppingListComponent }
 ])
 export class App {
   angularclassLogo = 'assets/img/angularclass-avatar.png';
   loading = false;
   name = 'Angular 2 Webpack Starter';
   url = 'https://twitter.com/AngularClass';
+  public isCollapsed: boolean;
 
   constructor(
     public appState: AppState) {
@@ -64,6 +89,7 @@ export class App {
 
   ngOnInit() {
     console.log('Initial App State', this.appState.state);
+    this.isCollapsed = false;
   }
 
 }

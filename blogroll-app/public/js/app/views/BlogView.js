93981f28ab3404ec8f227bd9112aff14a584ce89
Siarhei <sv.siarhei@gmail.com>
2016-06-07 18:33:01 +0300

    Blogroll App: Blog edit ability

--- blogroll-app/public/js/app/views/BlogView.js
+++ blogroll-app/public/js/app/views/BlogView.js
@@ -19,15 +19,36 @@ define([
       // View constructor
       initialize: function() {
 
+        this.model.on("change", this.render, this);
         this.render();
 
       },
 
       // View Event Handlers
       events: {
-
+        "click .edit-button": "edit",
+        "click .update-button": "update"
       },
-      
+
+      edit: function() {
+        var author = this.$(".author").text();
+        var title = this.$(".title").text();
+        var url = this.$(".url").text();
+        this.$(".author").html("<input type=\"text\" class=\"edit-author form-control\" value=\"" + author + "\" />");
+        this.$(".title").html("<input type=\"text\" class=\"edit-title form-control\" value=\"" + title + "\" />");
+        this.$(".url").html("<input type=\"text\" class=\"edit-url form-control\" value=\"" + url + "\" />");
+        this.$(".update-button, .cancel-button").removeClass("hide");
+        this.$(".edit-button, .delete-button").addClass("hide");
+      },
+
+      update: function() {
+        this.model.set({
+          author: $('.edit-author').val(),
+          title: $('.edit-title').val(),
+          url: $('.edit-url').val()
+        });
+      },
+
       render: function() {
 
         this.template = Mustache.render(template, this.model.toJSON());

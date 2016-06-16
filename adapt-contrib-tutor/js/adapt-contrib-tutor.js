607e0cad34fd5ac42bce0135cefedebc1a72b8a7
Siarhei Savitski <siarhei.savitski@instinctools.ru>
2016-06-16 11:12:55 +0300

    RM-264 Styling the "Tutor" extension #comment Add stylization for popup notifications

--- adapt-contrib-tutor/js/adapt-contrib-tutor.js
+++ adapt-contrib-tutor/js/adapt-contrib-tutor.js
@@ -10,19 +10,26 @@ define([
         };
 
         if (view.model.has('_isCorrect')) {
+            var answerIndicator = '';
             // Attach specific classes so that feedback can be styled.
             if (view.model.get('_isCorrect')) {
+                answerIndicator = '<b>Correct! </b>';
                 alertObject._classes = 'correct';
             } else {
                 if (view.model.has('_isAtLeastOneCorrectSelection')) {
                     // Partially correct feedback is an option.
+                    answerIndicator = view.model.get('_isAtLeastOneCorrectSelection')
+                        ? '<b>Partially correct! </b>'
+                        : '<b>Incorrect! </b>';
                     alertObject._classes = view.model.get('_isAtLeastOneCorrectSelection')
                         ? 'partially-correct'
                         : 'incorrect';
                 } else {
+                    answerIndicator = '<b>Incorrect! </b>';
                     alertObject._classes = 'incorrect';
                 }
             }
+            alertObject.title = answerIndicator + alertObject.title;
         }
 
         Adapt.once("notify:closed", function() {

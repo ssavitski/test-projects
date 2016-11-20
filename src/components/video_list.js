a0ca4a7014bf544d5e2f6ea1ca378c12e58b5ca3
ssavitski <sv.siarhei@gmail.com>
2016-11-20 06:51:26 +0200

    Add video list item component

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import VideoListItem from './video_list_item';
 
-const VideoList = (props) => (
-  <ul className="col-md-4 list-group">
-    {props.videos.length}
-  </ul>
-);
+
+const VideoList = (props) => {
+  const videoItems = props.videos.map(video => <VideoListItem
+    key={video.etag}
+    video={video}
+  />);
+
+  return (
+    <ul className="col-md-4 list-group">
+      {videoItems}
+    </ul>
+  );
+};
 
 export default VideoList;

9417bf77eed14eef23c63a186c6fabc210b40a72
Siarhei Savitski <sv.siarhei@gmail.com>
2016-12-28 18:37:38 +0300

    Add chart representation of data

--- src/containers/weather-list.js
+++ src/containers/weather-list.js
@@ -1,29 +1,35 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+import Chart from '../components/chart';
+
 class WeatherList extends Component {
   renderWeather(cityData, index) {
     const name = cityData.city.name;
 
+    const temps = cityData.list.map(weather => weather.main.temp);
+    const pressures = cityData.list.map(weather => weather.main.pressure);
+    const humidities = cityData.list.map(weather => weather.main.humidity);
+
     return (
       <tr key={index}>
         <td>{name}</td>
-        <td></td>
-        <td></td>
-        <td></td>
+        <td className="align-bottom"><Chart data={temps} color="orange" units="K" /></td>
+        <td className="align-bottom"><Chart data={pressures} color="green" units="hPa" /></td>
+        <td className="align-bottom"><Chart data={humidities} color="black" units="%" /></td>
       </tr>
-    )
+    );
   }
 
   render() {
     return (
-      <table className="table table-hover table-striped">
+      <table className="table table-hover table-striped weather-list">
         <thead>
           <tr className="warning">
             <th>City</th>
-            <th>Tempreture</th>
-            <th>Pressure</th>
-            <th>Humidity</th>
+            <th>Tempreture (K)</th>
+            <th>Pressure (hPa)</th>
+            <th>Humidity (%)</th>
           </tr>
         </thead>
 
